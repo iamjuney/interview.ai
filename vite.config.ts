988085d680253731248a6c7972fcf28bfea4f1c2
Aljon Lerios <aljonclerios@gmail.com>
2024-02-02 21:32:50 +0800

    Add dotenv and dotenv-expand dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import dotenvExpand from 'dotenv-expand';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	optimizeDeps: {
-		exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
-	},
-	server: {
-		headers: {
-			'Cross-Origin-Opener-Policy': 'same-origin',
-			'Cross-Origin-Embedder-Policy': 'require-corp'
-		}
+export default defineConfig(({ mode }) => {
+	// This check is important!
+	if (mode === 'development') {
+		const env = loadEnv(mode, process.cwd(), '');
+		dotenvExpand.expand({ parsed: env });
 	}
+
+	return {
+		plugins: [sveltekit()],
+		optimizeDeps: {
+			exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+		},
+		server: {
+			headers: {
+				'Cross-Origin-Opener-Policy': 'same-origin',
+				'Cross-Origin-Embedder-Policy': 'require-corp'
+			}
+		}
+	};
 });
