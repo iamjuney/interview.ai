c72d029c5218355d248ac900fae6d9178ccf7597
Aljon Lerios <aljonclerios@gmail.com>
2024-02-03 11:08:15 +0800

    Add interview, question, answer, and assessment tables to the database schema

--- src/lib/db/schema/index.ts
+++ src/lib/db/schema/index.ts
@@ -1,4 +1,13 @@
-import { pgTable, bigint, varchar, timestamp, text, uniqueIndex } from 'drizzle-orm/pg-core';
+import {
+	pgTable,
+	bigint,
+	varchar,
+	timestamp,
+	text,
+	uniqueIndex,
+	integer,
+	json
+} from 'drizzle-orm/pg-core';
 
 export const user = pgTable(
 	'users',
@@ -56,4 +65,81 @@ export const key = pgTable('user_key', {
 	hashedPassword: varchar('hashed_password', {
 		length: 255
 	})
+});
+
+export const interview = pgTable('interviews', {
+	id: varchar('id', {
+		length: 36
+	}).primaryKey(),
+	title: varchar('title', {
+		length: 255
+	}).notNull(),
+	description: text('description').notNull(),
+	company: varchar('company', {
+		length: 255
+	}).notNull(),
+	questions: varchar('questions', {
+		length: 36
+	})
+		.notNull()
+		.array(),
+	createdAt: timestamp('createdAt').defaultNow().notNull()
+});
+
+export const question = pgTable('questions', {
+	id: varchar('id', {
+		length: 36
+	}).primaryKey(),
+	question: text('question').notNull(),
+	duration: integer('duration').notNull(),
+	videoUrl: text('video_url').default(''),
+	interviewId: varchar('interview_id', {
+		length: 36
+	})
+		.notNull()
+		.references(() => interview.id, { onDelete: 'cascade' }),
+	createdAt: timestamp('createdAt').defaultNow().notNull()
+});
+
+export const answer = pgTable('answers', {
+	id: varchar('id', {
+		length: 36
+	}).primaryKey(),
+	answer: text('answer').notNull(),
+	videoUrl: text('video_url').default(''),
+	userId: varchar('user_id', {
+		length: 36
+	})
+		.notNull()
+		.references(() => user.id, { onDelete: 'cascade' }),
+	questionId: varchar('question_id', {
+		length: 36
+	})
+		.notNull()
+		.references(() => question.id, { onDelete: 'cascade' }),
+	createdAt: timestamp('createdAt').defaultNow().notNull()
+});
+
+export const assessment = pgTable('assessment', {
+	id: varchar('id', {
+		length: 36
+	}).primaryKey(),
+	userId: varchar('user_id', {
+		length: 36
+	})
+		.notNull()
+		.references(() => user.id, { onDelete: 'cascade' }),
+	questionId: varchar('question_id', {
+		length: 36
+	})
+		.notNull()
+		.references(() => question.id, { onDelete: 'cascade' }),
+	feedback: text('feedback').notNull(),
+	wpm: integer('wpm').notNull(),
+	accuracy: integer('accuracy').notNull(),
+	pronunciation: integer('pronunciation').notNull(),
+	fluency: integer('fluency').notNull(),
+	mispronunciation: integer('mispronunciation').notNull(),
+	data: json('data').default({}),
+	createdAt: timestamp('createdAt').defaultNow().notNull()
 });
