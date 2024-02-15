849e1385aa307395917d20a9a46f5bdfad0f0d13
Aljon Lerios <aljonclerios@gmail.com>
2024-02-15 11:03:50 +0800

    Revert adapter change in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
