3ac6e06dc88e66915d1eb8e1ad993885b5c18287
Aljon Lerios <aljonclerios@gmail.com>
2024-02-16 14:32:30 +0800

    Refactor POST request in assessments server file

--- src/routes/(api)/api/db/assessments/+server.ts
+++ src/routes/(api)/api/db/assessments/+server.ts
@@ -2,16 +2,35 @@ import { db } from '$lib/db';
 import { assessment } from '$lib/db/schema';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { sql } from 'drizzle-orm';
 
 export const config = {
 	runtime: 'edge'
 };
 
 export const POST: RequestHandler = async ({ request }) => {
-	const form = await request.json();
+	const {
+		id,
+		answerId,
+		feedback,
+		accuracy_score,
+		pronunciation_score,
+		fluency_score,
+		prosody_score,
+		data
+	} = await request.json();
 
 	try {
-		await db.insert(assessment).values(form);
+		await db.insert(assessment).values({
+			id: id,
+			answerId: answerId,
+			feedback: feedback,
+			accuracy_score: accuracy_score,
+			pronunciation_score: pronunciation_score,
+			fluency_score: fluency_score,
+			prosody_score: prosody_score,
+			data: sql`${data}::jsonb`
+		});
 	} catch (error) {
 		return json({ error: 'Error saving assessments to database' }, { status: 400 });
 	}
