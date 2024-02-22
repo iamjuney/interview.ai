9607e34612ba67a13de5cdc3798117c279240713
Aljon Lerios <aljonclerios@gmail.com>
2024-02-22 09:50:06 +0800

    Update Cloudinary API key and remove Vercel Blob server code

--- src/routes/(api)/api/services/cloudinary/+server.ts
+++ src/routes/(api)/api/services/cloudinary/+server.ts
@@ -1,4 +1,4 @@
-import { CLOUDINARY_API_SECRET, VITE_PUBLIC_CLOUDINARY_API_KEY } from '$env/static/private';
+import { CLOUDINARY_API_SECRET, CLOUDINARY_API_KEY } from '$env/static/private';
 import { PUBLIC_CLOUDINARY_CLOUD_NAME } from '$env/static/public';
 import { error, json, type RequestHandler } from '@sveltejs/kit';
 import { v2 as cloudinary } from 'cloudinary';
@@ -6,7 +6,7 @@ import fs from 'fs';
 
 cloudinary.config({
 	cloud_name: PUBLIC_CLOUDINARY_CLOUD_NAME,
-	api_key: VITE_PUBLIC_CLOUDINARY_API_KEY,
+	api_key: CLOUDINARY_API_KEY,
 	api_secret: CLOUDINARY_API_SECRET
 });
 
