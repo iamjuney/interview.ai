08f2e49405287e4c5dff08d91035ad81e7f8ac80
Aljon Lerios <aljonclerios@gmail.com>
2024-04-20 07:45:55 +0800

    Update font preload links in app.html and app.pcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,7 +55,7 @@ const config = {
 				sm: 'calc(var(--radius) - 4px)'
 			},
 			fontFamily: {
-				sans: ['DM Sans', 'Geist', ...fontFamily.sans]
+				sans: ['DM Sans', ...fontFamily.sans]
 			},
 			scale: {
 				'-1': '-1'
