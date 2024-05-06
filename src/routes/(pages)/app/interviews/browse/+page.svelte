1587db367b783237c0674d1ccf22500e8057532a
Aljon Lerios <aljonclerios@gmail.com>
2024-05-06 18:56:59 +0800

    feat: Add dropdown menu for filtering interviews by difficulty level

--- src/routes/(pages)/app/interviews/browse/+page.svelte
+++ src/routes/(pages)/app/interviews/browse/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { Input, InterviewCard, NotFound } from '$lib/components';
+	import { Input, InterviewCard, NotFound, DropdownMenu, Button } from '$lib/components';
 	import type { SubmitFunction } from '@sveltejs/kit';
-	import { ArrowLeft, Loader2, Search } from 'lucide-svelte';
+	import { ArrowLeft, Loader2, Search, ListFilter } from 'lucide-svelte';
 	import { backOut } from 'svelte/easing';
 	import { fly } from 'svelte/transition';
 
@@ -14,9 +14,32 @@
 	let timeoutId = $state<NodeJS.Timeout>();
 	let isSearching = $state(false);
 	let failedSearchData = $state<Record<string, any>>();
+	let selectedFilter = $state<string>('All');
 
 	$effect(() => {
 		animate = true;
+	});
+
+	$effect(() => {
+		if (query.length === 0) {
+			if (selectedFilter === 'All') {
+				interviews = data.interviews;
+			} else {
+				interviews = data.interviews.filter((interview) => interview.difficulty === selectedFilter);
+			}
+		} else {
+			if (selectedFilter === 'All') {
+				interviews = data.interviews.filter((interview) =>
+					interview.position.toLowerCase().includes(query.toLowerCase())
+				);
+			} else {
+				interviews = data.interviews.filter(
+					(interview) =>
+						interview.position.toLowerCase().includes(query.toLowerCase()) &&
+						interview.difficulty === selectedFilter
+				);
+			}
+		}
 	});
 
 	// fly transition options
@@ -73,32 +96,70 @@
 		</div>
 
 		<div class="h-full">
-			<form
-				bind:this={searchForm}
-				class="w-full"
-				use:enhance={handleSubmit}
-				action="?/search"
-				method="post"
-			>
-				<label for="query" class="sr-only">Search</label>
-				<div class="relative">
-					<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-0">
-						{#if isSearching}
-							<Loader2 size="20" class="animate-spin text-muted-foreground" />
-						{:else}
-							<Search size="20" class="text-muted-foreground" />
-						{/if}
+			<div class="flex items-center justify-between">
+				<form
+					bind:this={searchForm}
+					class="w-full"
+					use:enhance={handleSubmit}
+					action="?/search"
+					method="post"
+				>
+					<label for="query" class="sr-only">Search</label>
+					<div class="relative">
+						<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-0">
+							{#if isSearching}
+								<Loader2 size="20" class="animate-spin text-muted-foreground" />
+							{:else}
+								<Search size="20" class="text-muted-foreground" />
+							{/if}
+						</div>
+						<Input
+							bind:value={query}
+							oninput={handleInput}
+							id="query"
+							name="query"
+							class="max-w-sm border-0 pl-8 text-base"
+							placeholder="Search"
+						/>
 					</div>
-					<Input
-						bind:value={query}
-						oninput={handleInput}
-						id="query"
-						name="query"
-						class="max-w-sm border-0 pl-8 text-base"
-						placeholder="Search"
-					/>
-				</div>
-			</form>
+				</form>
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger asChild let:builder>
+						<Button variant="outline" size="sm" class="h-7 gap-1 text-sm" builders={[builder]}>
+							<ListFilter class="h-3.5 w-3.5" />
+							<span class="sr-only sm:not-sr-only">Filter</span>
+						</Button>
+					</DropdownMenu.Trigger>
+					<DropdownMenu.Content align="end">
+						<DropdownMenu.Label>Filter by Difficulty</DropdownMenu.Label>
+						<DropdownMenu.Separator />
+						<DropdownMenu.CheckboxItem
+							checked={selectedFilter === 'All'}
+							onCheckedChange={() => {
+								selectedFilter = 'All';
+							}}>All</DropdownMenu.CheckboxItem
+						>
+						<DropdownMenu.CheckboxItem
+							checked={selectedFilter === 'Basic'}
+							onCheckedChange={() => {
+								selectedFilter = 'Basic';
+							}}>Basic</DropdownMenu.CheckboxItem
+						>
+						<DropdownMenu.CheckboxItem
+							checked={selectedFilter === 'Intermediate'}
+							onCheckedChange={() => {
+								selectedFilter = 'Intermediate';
+							}}>Intermediate</DropdownMenu.CheckboxItem
+						>
+						<DropdownMenu.CheckboxItem
+							checked={selectedFilter === 'Advanced'}
+							onCheckedChange={() => {
+								selectedFilter = 'Advanced';
+							}}>Advanced</DropdownMenu.CheckboxItem
+						>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+			</div>
 
 			{#if failedSearchData}
 				<p class="text-muted-foreground">{failedSearchData.message}</p>
